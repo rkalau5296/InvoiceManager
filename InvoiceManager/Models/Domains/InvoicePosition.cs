6dc82c2820e2c12db7d385f7c2b39c883ff1f3b1
rafal <rafal.kalata.itservice@gmail.com>
2023-05-15 23:03:17 +0200

    commit

--- InvoiceManager/Models/Domains/InvoicePosition.cs
+++ InvoiceManager/Models/Domains/InvoicePosition.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Web;
@@ -11,9 +12,12 @@ namespace InvoiceManager.Models.Domains
         public int Id { get; set; }
         public int Lp { get; set; }
         public int InvoiceId { get; set; }
+        [Display(Name = "Wartość")]
         public decimal Value { get; set; }
+        [Display(Name = "Ilość")]
         public int Quantity { get; set; }
         public Invoice Invoice { get; set; }
+        [Display(Name = "Produkt")]
         public Product Product { get; set; }        
         public int ProductId { get; set; }
     }
