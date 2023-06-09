298f4ae0b966f9b37d9bf310a079f790a256c057
rafal <rafal.kalata.itservice@gmail.com>
2023-06-09 13:21:37 +0200

    commit

--- InvoiceManager/App_Start/BundleConfig.cs
+++ InvoiceManager/App_Start/BundleConfig.cs
@@ -8,7 +8,8 @@ namespace InvoiceManager
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery.cookie.js",
+            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
