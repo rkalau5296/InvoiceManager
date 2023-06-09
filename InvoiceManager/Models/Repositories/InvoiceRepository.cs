108da0de201c4e226cd5aa0ae375560e8b78a7cf
rafal <rafal.kalata.itservice@gmail.com>
2023-06-09 11:29:27 +0200

    commit

--- InvoiceManager/Models/Repositories/InvoiceRepository.cs
+++ InvoiceManager/Models/Repositories/InvoiceRepository.cs
@@ -58,7 +58,7 @@ namespace InvoiceManager.Models.Repositories
         {
             using (var context = new ApplicationDbContext())
             {
-                invoice.CreatedDate= DateTime.Now;
+                //invoice.CreatedDate= DateTime.Now;
                 context.Invoices.Add(invoice);
                 context.SaveChanges();
             }
