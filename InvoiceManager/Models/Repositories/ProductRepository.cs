9cf497c4f9007052ef789733a1048e2315dddf34
Rafał Kalata <rafal.kalata.itservice@gmail.com>
2023-06-18 18:08:43 +0200

    commit

--- InvoiceManager/Models/Repositories/ProductRepository.cs
+++ InvoiceManager/Models/Repositories/ProductRepository.cs
@@ -6,19 +6,48 @@ namespace InvoiceManager.Models.Repositories
 {
     public class ProductRepository
     {
-        public List<Product> GetProducts()
+        public List<Product> GetProducts(string userId)
         {
             using (var context = new ApplicationDbContext())
             {
-                return context.Products.ToList();
+                return context.Products.Where(x=>x.UserId == userId).ToList();
             }
         }
 
-        public Product GetProduct(int productId)
+        public Product GetProduct(int productId, string userId)
         {
             using (var context = new ApplicationDbContext())
             {
-                return context.Products.Single(p => p.Id == productId);
+                return context.Products.Single(p => p.Id == productId && p.UserId == userId);
+            }
+        }
+        public void Add(Product product)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void Update(Product product)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var productToUpdate = context.Products.Single(x => x.Id == product.Id && x.UserId == product.UserId);
+
+                productToUpdate.Name = product.Name;
+                productToUpdate.Value = product.Value;
+                context.SaveChanges();
+            }
+        }
+        public void Delete(int id, string userId)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                var productToDelete = context.Products.Single(x => x.Id == id && x.UserId == userId);
+
+                context.Products.Remove(productToDelete);
+                context.SaveChanges();
             }
         }
     }
---
+++ InvoiceManager/Models/ViewModels/EditProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InvoiceManager.Models.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceManager.Models.ViewModels
+{
+    public class EditProductViewModel
+    {
+        public string Heading { get; set; }
+        public Product Product { get; set; }
+    }
+}
